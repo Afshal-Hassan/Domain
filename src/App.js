1a213f0e5bbff2a1b2e4adf3e3b2b8b3b7bd2494
Afshal Hassan <afshal.hassan@venturedive.com>
2023-02-25 23:41:11 +0500

    Version 0.0

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{
+          fontFamily:"Averta, sans-serif"
+        }}>
+          NexaLab
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          style={{
+          fontFamily:"Averta, sans-serif"
+        }}
         >
-          Learn React
+           Coming Soon
         </a>
       </header>
     </div>
